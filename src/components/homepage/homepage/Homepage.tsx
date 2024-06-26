989d54638d07a11006e5eac961f507fa5411a08f
FARHAN AHMAD <117432911+789farhan@users.noreply.github.com>
2024-06-26 17:59:48 +0500

    login chat

--- src/components/homepage/homepage/Homepage.tsx
+++ src/components/homepage/homepage/Homepage.tsx
@@ -172,9 +172,12 @@ export default function Homepage() {
                       className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2"
                     >
                       <div className="flex items-center justify-center h-8 w-8 bg-purple-200 rounded-full">
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        {/* @ts-ignore */}
                         <img src={user.avatar} alt="" />
                       </div>
                       <div className="ml-2 text-sm font-semibold">
+                        {/* @ts-ignore */}
                         {user.name}
                       </div>
                     </button>
